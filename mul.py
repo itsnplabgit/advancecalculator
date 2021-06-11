b78e9c94a3d2d6046f040259c88f290607eb574b
Saurav Ghimire <saurav.ghimire10@gmail.com>
2021-06-11 17:55:03 +0545

    2nd version

--- mul.py
+++ mul.py
@@ -1,1 +1,4 @@
-# Mul Work
+a = int (input ("Enter 1st number: "))
+b = int (input ("Enter 2nd number: "))
+c = a * b
+print ("Multiplication is: ", c)
--- sum.py
+++ sum.py
@@ -1,1 +1,4 @@
-# Sum
+a = int (input ("Enter 1st number: "))
+b = int (input ("Enter 2nd number: "))
+c = a + b
+print ("Sum is: ", c)
